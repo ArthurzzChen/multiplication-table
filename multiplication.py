e48775c9f8046a96db6a4cdc3d919949711168e9
arthur chen <arthurzz@gmail.com>
2025-02-27 08:39:17 +0800

    version 2 test for github

--- multiplication.py
+++ multiplication.py
@@ -1,3 +1,5 @@
+print("Multiplication Table ver.2")
+print("-----------------------------------------------------------------------------------------------")
 for i in range(1, 10):
     for j in range(1, 10):
         print(f"{i} x {j} = {i*j}", end="\t")
